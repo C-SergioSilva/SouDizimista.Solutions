350cec9137742ea2b19b3aeb93228e95812812bb
carlos sergio <carlos.silva@tty2000.com.br>
2023-05-16 18:43:17 -0300

    Ajustando layout Principal e formulário Adicionar Dizimista

--- SouDizimista.WebApp/wwwroot/js/manu_lateral.js
+++ SouDizimista.WebApp/wwwroot/js/manu_lateral.js
@@ -1,24 +1,24 @@
 ﻿var list = document.querySelectorAll('.list');
 
-//function ActiveLink() {
-//    list.forEach((item) =>
-//        item.classList.remove('active'));
-//    this.classList.add('active');
+function ActiveLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
 
-//}
+}
 
-//list.forEach((item) =>
-//    item.addEventListener('click', ActiveLink)
-//);
+list.forEach((item) =>
+    item.addEventListener('click', ActiveLink)
+);
 
 let menuList = document.querySelectorAll('#paroquias-menu');
 
 
-//menuList.forEach((item) =>
-//    item.addEventListener('click', showMenuLateral)    
-//);
+menuList.forEach((item) =>
+    item.addEventListener('click', showMenuLateral)    
+);
 
-//function showMenuLateral() {
-//    menuList.forEach((item) =>
-//        document.querySelector('.navigation').style.display = 'block');
-//}
+function showMenuLateral() {
+    menuList.forEach((item) =>
+        document.querySelector('.navigation').style.display = 'block');
+}
